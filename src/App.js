2aa4409568a4eff2c29d8e385043bb9c6440b5fc
mugglekim <shalomseonmi@gmail.com>
2025-01-21 18:00:16 +0900

    update component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.scss';
+import Banner from './components/Banner';
+import FAQ from './components/FAQ';
+import Header from './components/Header';
 const App = () => {
   return (
-    <div>
-      <h1>넷플릭스 클론사이트</h1>
+    <div className='app'>
+      <Header/>
+      <Banner/>
+      <FAQ/>
     </div>
   );
 };
